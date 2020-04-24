2c860baa6db512133eb0df48c7ad8b8cb4f803d1
nathan <nathankfoss@gmail.com>
2020-04-24 12:24:22 -0500

    Implement ViewChild alternative for form access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,9 +8,19 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
 
-  onSubmit(form: NgForm) {
-    console.log(form);
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  // This is just an alternative to the commented out code above.
+  // This is useful we need to access the form earlier than the moment we submit it.
+  // Remember to use static: true, for Angular 8+
+  @ViewChild('bandit', {static: true}) signUpForm: NgForm;
+
+  onSubmit() {
+    console.log(this.signUpForm)
   }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
